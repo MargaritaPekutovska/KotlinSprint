6338c71bb2cb35ab48ad81fef3c3e530fe7f5269
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-06-13 15:13:05 +0200

    KS-1-2 added number of employees. changed val to var

--- src/main/kotlin/lesson1Task2.kt
+++ src/main/kotlin/lesson1Task2.kt
@@ -5,9 +5,9 @@ fun main() {
     val textOfGratitude: String = "Thank for your order in our store!"
     println("number Of Orders=$numberOfOrders, text Of Gratitude= $textOfGratitude")
 
-    /*val numberOfEmployees: Int = 2000
+    /*var numberOfEmployees: Int = 2000
     println("number of employees=$numberOfEmployees")*/
 
-    val numberOfEmployees: Int = 1999
+    var numberOfEmployees: Int = 1999
     println("number of employees=$numberOfEmployees")
 }
