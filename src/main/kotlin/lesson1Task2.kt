cebf1659d8023ebdb7372c63c9b07451c17e208a
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-06-13 15:01:21 +0200

    KS-1-2 added number of employees

---
+++ src/main/kotlin/lesson1Task2.kt
@@ -0,0 +1,13 @@
+package org.example
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val textOfGratitude: String = "Thank for your order in our store!"
+    println("number Of Orders=$numberOfOrders, text Of Gratitude= $textOfGratitude")
+
+    /*val numberOfEmployees: Int = 2000
+    println("number of employees=$numberOfEmployees")*/
+
+    val numberOfEmployees: Int = 1999
+    println("number of employees=$numberOfEmployees")
+}
