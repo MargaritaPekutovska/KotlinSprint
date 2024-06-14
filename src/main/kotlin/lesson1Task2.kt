d4d3550ac5538a570a07139dcdbe19c618dad35a
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-06-14 12:44:05 +0200

    KS-1-2 committed the number of var-2000

--- src/main/kotlin/lesson1Task2.kt
+++ src/main/kotlin/lesson1Task2.kt
@@ -5,9 +5,8 @@ fun main() {
     val textOfGratitude: String = "Thank for your order in our store!"
     println("number Of Orders=$numberOfOrders, text Of Gratitude= $textOfGratitude")
 
-    /*var numberOfEmployees: Int = 2000
-    println("number of employees=$numberOfEmployees")*/
-
-    var numberOfEmployees: Int = 1999
+    var numberOfEmployees: Int
+    //numberOfEmployees=2000
+    numberOfEmployees = 1999
     println("number of employees=$numberOfEmployees")
 }
