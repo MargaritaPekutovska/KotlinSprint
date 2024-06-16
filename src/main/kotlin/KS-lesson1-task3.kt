4bf3df907de262e01812f2924bdc0fad13b0cb63
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-06-16 19:37:25 +0200

    KS-1-4 details of the Space travel

--- src/main/kotlin/KS-lesson1-task3.kt
+++ src/main/kotlin/KS-lesson1-task3.kt
@@ -1,19 +1,6 @@
-/*fun main() {
-    val yearOfSpaceFlight: Int = 1961
-    var takeOffHour: String = "09"
-    var takeOffMinute: String = "07"
-    println(
-        "Year of space flight: $yearOfSpaceFlight \n " +
-                "Take off hour: $takeOffHour \n " +
-                "Take off minute: $takeOffMinute"
-    )
-
-    var landingHour: String = "10"
-    var landingMinute: String = "55"
-    println("Landing time: $landingHour:$landingMinute")
-}*/
-fun main(){
+fun main() {
     trimMargin()
+    detailsOfSpaceTravel()
 }
 
 fun trimMargin() {
@@ -21,7 +8,7 @@ fun trimMargin() {
     var takeOffHour: String = "09"
     var takeOffMinute: String = "07"
 
-    val multiString= """
+    val multiString = """
         |Year of space flight: $yearOfSpaceFlight
         |Take off hour: $takeOffHour
         |Take off minute: $takeOffMinute
@@ -31,5 +18,24 @@ fun trimMargin() {
     var landingHour: String = "10"
     var landingMinute: String = "55"
     println("Landing time: $landingHour:$landingMinute")
+}
+
+fun detailsOfSpaceTravel() {
+    val flightLength: Long = 40_868_600_000
+    val ageOfAstronaut: Byte = 27
+    val partOfDay: Float = 0.075f
+    val second: Short = 6480
+    val partOfTheYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    val spaceTravelInfo = """
+        |Fly distance in mm: $flightLength
+        |Gagarin's age at the time of the flight: $ageOfAstronaut
+        |Part of the day in Space: $partOfDay
+        |Time in second which astronaut spent in Space: $second
+        |Season of the year when Gagarin was in Space: $partOfTheYear
+        |Apogee of Gagarin's orbit in meters: $apogee
+        """.trimMargin()
+    println(spaceTravelInfo)
 }
 
