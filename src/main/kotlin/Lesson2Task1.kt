bd38a9b728b0ef8c60ec978f36b21c3acd40fd8a
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-06-21 23:47:23 +0200

    KS-2-1 GPA in english

---
+++ src/main/kotlin/Lesson2Task1.kt
@@ -0,0 +1,15 @@
+fun main() {
+    englishClass()
+}
+
+fun englishClass() {
+    val gradeStudent1 = 3
+    val gradeStudent2 = 4
+    val gradeStudent3 = 3
+    val gradeStudent4 = 5
+
+    val sum = (
+            gradeStudent1 + gradeStudent2 + gradeStudent3 + gradeStudent4
+            ) / 4
+    println("GPA= $sum")
+}
