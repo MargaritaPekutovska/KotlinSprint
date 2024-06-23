01712c955d641fd8ac26176387228582d8c29016
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-06-23 22:42:11 +0200

    KS-2-4 Fixed Comments

--- src/main/kotlin/Lesson2-Task4.kt
+++ src/main/kotlin/Lesson2-Task4.kt
@@ -1,15 +1,16 @@
 fun main() {
-    mobileStrategy()
+    mobileStrategy(20)
 }
 
-fun mobileStrategy() {
+const val ONE_HUNDRED_PERCENT = 100
+
+fun mobileStrategy(buffUnitBooster: Int) {
     val crystallineMaterial = 7
     val ironOre = 11
-    val buffUnitBooster = 0.20f
 
-    val cristalBonus = crystallineMaterial * buffUnitBooster
+    val cristalBonus = (crystallineMaterial * buffUnitBooster) / ONE_HUNDRED_PERCENT
     println("Cristal line Material bonus ${cristalBonus}")
 
-    val ironBonus = ironOre * buffUnitBooster
+    val ironBonus = (ironOre * buffUnitBooster) / ONE_HUNDRED_PERCENT
     println("Iron ore bonus ${ironBonus}")
 }
