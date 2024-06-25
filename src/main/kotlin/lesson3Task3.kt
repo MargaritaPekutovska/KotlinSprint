c02bb585826324566c7727a193efda0634d0a61f
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-06-25 23:07:33 +0200

    KS lesson3-3 multiplication table

---
+++ src/main/kotlin/lesson3Task3.kt
@@ -0,0 +1,11 @@
+fun main() {
+    multiplicationTableApp(inputNumber = 5)
+}
+
+fun multiplicationTableApp(inputNumber: Int) {
+    var result= ""
+    for (i in 1..9) {
+        result += "$inputNumber x $i = ${inputNumber * i} \n"
+    }
+    println(result)
+}
