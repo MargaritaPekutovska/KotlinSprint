362bd8a7e76fd41b2bb3c9ca9de5853471ef8d58
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-06-29 23:00:35 +0200

    KS lesson5-Task2 Age verification

---
+++ src/main/kotlin/Lesson5Task2.kt
@@ -0,0 +1,17 @@
+fun main() {
+    ageVerification(2005)
+}
+
+const val AGE_OF_MAJORITY = 18
+
+fun ageVerification(userAge: Int) {
+    var currentYear: Int = 2024
+    val yearOfMajority = currentYear - AGE_OF_MAJORITY
+
+    if (userAge >= yearOfMajority)
+        println("Show hidden content")
+    else if (userAge == 2004 || userAge == 2005)
+        println("Show limited content")
+    else
+        println("Return to home page")
+}
