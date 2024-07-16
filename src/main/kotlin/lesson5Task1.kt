bc829491fd7b46497753d27d2636c47de5e49331
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2024-07-16 11:23:49 +0700

    KS lesson 5-Task 1 replaced readline with readln

--- src/main/kotlin/lesson5Task1.kt
+++ src/main/kotlin/lesson5Task1.kt
@@ -2,8 +2,8 @@ const val LOGIN_DATA = 20
 
 fun main() {
     println("Please enter your login data")
-    val firstNumber = readLine()!!.toInt()
-    val secondNumber = readLine()!!.toInt()
+    val firstNumber = readln().toInt()
+    val secondNumber = readln().toInt()
     if ((firstNumber + secondNumber) == LOGIN_DATA) {
         println("Welcome!")
     } else {
