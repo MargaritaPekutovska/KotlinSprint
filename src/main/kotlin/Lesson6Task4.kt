6af6a83031ed905ebfa7258c410af763b50e707a
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2024-07-18 00:53:09 +0700

    KS lesson 6 Task 4 game app

---
+++ src/main/kotlin/Lesson6Task4.kt
@@ -0,0 +1,29 @@
+import kotlin.random.Random
+
+fun main() {
+    guessNumberGame()
+}
+
+const val MINIMUM_GUESS_NUMBER = 1
+const val MAX_GUESS_NUMBER = 9
+const val MAX_TRIES_NUMBER = 5
+
+fun guessNumberGame() {
+    val targetNumber = Random.nextInt(MINIMUM_GUESS_NUMBER, MAX_GUESS_NUMBER)
+    var triesLeft = MAX_TRIES_NUMBER
+    println("You have 5 tries to guess winning number")
+
+    while (triesLeft > 0) {
+        println("Write your guess number")
+        val userNumber = readln().toInt()
+        triesLeft--
+
+        if (userNumber == targetNumber) {
+            println("It was a marvelous game!")
+            break
+        } else {
+            println("Wrong number!")
+        }
+    }
+    println("The winning number was $targetNumber")
+}
