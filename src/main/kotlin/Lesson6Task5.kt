f615e9dd1bd6f6e7d52d22f98d0e86fb8455f64b
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2024-07-20 12:27:22 +0700

    KS Lesson 6 Task 5 resolved commends

--- src/main/kotlin/Lesson6Task5.kt
+++ src/main/kotlin/Lesson6Task5.kt
@@ -8,12 +8,14 @@ const val MINIMAL_NUMBER = 1
 const val MAXIMAL_NUMBER = 9
 const val TRIES_NUMBER = 3
 
+val rangeOfNumbers = IntRange(MINIMAL_NUMBER, MAXIMAL_NUMBER)
+
 fun loginVerification() {
     var triesNumber = TRIES_NUMBER
 
     while (triesNumber > 0) {
-        val firstRandomNumber = Random.nextInt(MINIMAL_NUMBER, MAXIMAL_NUMBER + 1)
-        val secondRandomNumber = Random.nextInt(MINIMAL_NUMBER, MAXIMAL_NUMBER + 1)
+        val firstRandomNumber = rangeOfNumbers.random()
+        val secondRandomNumber = rangeOfNumbers.random()
         println(
             "To prove that you are not a bot, please solve this simple exercise: " +
                     "What is the sum of $firstRandomNumber + $secondRandomNumber =..."
