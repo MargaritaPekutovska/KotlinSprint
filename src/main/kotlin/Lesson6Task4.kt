70ea3bcbb7f14b7fea794df5dcbfbb59be17d165
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2024-07-20 12:34:04 +0700

    KS Lesson 6 Task 4 resolved commends

--- src/main/kotlin/Lesson6Task4.kt
+++ src/main/kotlin/Lesson6Task4.kt
@@ -8,8 +8,10 @@ const val MINIMUM_GUESS_NUMBER = 1
 const val MAX_GUESS_NUMBER = 9
 const val MAX_TRIES_NUMBER = 5
 
+val rangeOfNumbers = IntRange(MINIMUM_GUESS_NUMBER, MAX_GUESS_NUMBER)
+
 fun guessNumberGame() {
-    val targetNumber = Random.nextInt(MINIMUM_GUESS_NUMBER, MAX_GUESS_NUMBER+1)
+    val targetNumber = rangeOfNumbers.random()
     var triesLeft = MAX_TRIES_NUMBER
     println("You have 5 tries to guess winning number")
 
