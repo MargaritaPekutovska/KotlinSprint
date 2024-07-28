86071633f7c82e71f250f730db6a63d30cbe14c8
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2024-07-28 23:47:47 +0700

    lesson 8 task 2 Recipes App

---
+++ src/main/kotlin/Lesson8Task2.kt
@@ -0,0 +1,21 @@
+fun main() {
+    recipesApp()
+}
+
+fun recipesApp() {
+    val arrayOfIngredients = arrayOf(
+        "salt", "chili pepper", "greenery", "lime", "coconut milk", "mushrooms", "garlic", "seafood"
+    )
+    println("Please write your own ingredient:")
+    val userIngredient = readln()
+
+    for (ingredient in arrayOfIngredients) {
+        if (userIngredient == ingredient) {
+            println("The recipe contains $ingredient")
+            break
+        }
+        if (arrayOfIngredients.indexOf(ingredient) == arrayOfIngredients.size - 1) {
+            println("The recipe doesn`t contains the ingredient $userIngredient")
+        }
+    }
+}
