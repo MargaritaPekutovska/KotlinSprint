f4efc0ad531b64bd8a108d3f0740c34dd6992c45
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2024-08-02 00:25:12 +0700

    lesson 8 task 5 changed the solution

--- src/main/kotlin/Lesson8Task5.kt
+++ src/main/kotlin/Lesson8Task5.kt
@@ -6,13 +6,10 @@ fun main() {
 
 private fun createYourOwnRecipe() {
     println("Please write the quantity of ingredients:")
-    val arraySize = readln().toInt()
 
-    val dataArray = arrayOfNulls<String>(arraySize)
-
-    for (i in 0 until arraySize) {
+    val dataArray = Array(readln().toInt()) {
         println("Please write your ingredient:")
-        dataArray[i] = readln()
+        readln()
     }
     println(Arrays.toString(dataArray))
 }
