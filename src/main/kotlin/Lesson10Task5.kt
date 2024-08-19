8ffc69177f617e8a9359a82ce6b8fa0ca1851531
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-08-19 15:52:38 +0200

    KS-Lesson10Task5 authorize user

---
+++ src/main/kotlin/Lesson10Task5.kt
@@ -0,0 +1,59 @@
+import java.util.Arrays
+
+fun main() {
+    shoppingCartApp()
+}
+
+private fun shoppingCartApp() {
+    val token: String? = authorizeUser()
+    val cart = getShoppingCart(token)
+    if (cart != null)
+        println(Arrays.toString(cart))
+    else println("Authorization not successful!")
+}
+
+private val shoppingCart: Array<String> = arrayOf("T-Shirt", "Shorts", "Cap")
+
+private fun getShoppingCart(token: String?): Array<String>? {
+    if (token == null) {
+        return null
+    } else {
+        return shoppingCart
+    }
+}
+
+private const val USER_NAME = "MargaritaP"
+private const val USER_PASSWORD = "sRt669"
+
+private fun authorizeUser(): String? {
+    println("Enter your login:")
+    val userLogin = readln()
+
+    println("Enter your password:")
+    val userPassword = readln()
+
+    if (userLogin == USER_NAME && userPassword == USER_PASSWORD) {
+        return generateToken()
+    } else {
+        return null
+    }
+
+}
+
+private const val TOKEN_LENGTH = 32
+
+private fun generateToken(): String {
+    val rangeLetters = ('a'..'z').toList()
+    val rangeNumbers = ('0'..'9').toList()
+    val tokenLength = TOKEN_LENGTH
+    val generateToken = StringBuilder(tokenLength)
+
+    for (i in 0 until tokenLength) {
+        if (i % 2 == 0) {
+            generateToken.append(rangeLetters.random())
+        } else {
+            generateToken.append(rangeNumbers.random())
+        }
+    }
+    return generateToken.toString()
+}
