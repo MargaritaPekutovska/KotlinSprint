52a5743208d33d42127a46ef9797172e705f1dda
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-08-23 14:35:33 +0200

    KS-Lesson11-Task1 added Id. Removed curly braces

--- src/main/kotlin/Lesson11_Task1_class_user.kt
+++ src/main/kotlin/Lesson11_Task1_class_user.kt
@@ -1,29 +1,32 @@
 class UserInfo(
-    var userName: String,
-    var userPassword: String,
-    var userEmail: String,
-) {
-
-}
+    val userName: String,
+    val userPassword: String,
+    val userId: String,
+    val userEmail: String,
+)
 
 fun main() {
     val userInfo1 = UserInfo(
         userName = "Mandalorian09",
         userPassword = "33356Sp1",
+        userId = "a2w45",
         userEmail = "mandalorian.inspace@gmail.com"
     )
 
     val userInfo2 = UserInfo(
         userName = "Grogu01",
         userPassword = "786Oj90R",
+        userId = "fdjkYYt45",
         userEmail = "grogu.inspace@gmail.com"
     )
 
     println(userInfo1.userName)
     println(userInfo1.userPassword)
+    println(userInfo1.userId)
     println(userInfo1.userEmail)
 
     println(userInfo2.userName)
     println(userInfo2.userPassword)
+    println(userInfo1.userId)
     println(userInfo2.userEmail)
 }
