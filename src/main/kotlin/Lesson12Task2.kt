61ca4c5e007ce06ac2441ae6999b521126392bf1
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-08-26 15:05:42 +0200

    KS-12-2 Daily Weather Update

---
+++ src/main/kotlin/Lesson12Task2.kt
@@ -0,0 +1,22 @@
+class DailyWeatherUpdate(
+    var daytimeTemperature: Int,
+    var nightTemperature: Int,
+    var precipitationDuringTheDay: Boolean
+) {
+    fun printWeather() {
+        println("Daytime temperature is: $daytimeTemperature°C")
+        println("Night temperature is: $nightTemperature°C")
+        println("Precipitation during the day: ${if (precipitationDuringTheDay) "yes" else "no"}")
+    }
+}
+
+fun main() {
+    val sundayWeather = DailyWeatherUpdate(
+        daytimeTemperature = 35,
+        nightTemperature = 28,
+        precipitationDuringTheDay = false
+    )
+
+    println("Weather on Sunday will be:")
+    sundayWeather.printWeather()
+}
