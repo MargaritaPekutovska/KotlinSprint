b7a488494b79996b9a8c3a2ff205c6b423a184b1
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-08-28 08:02:01 +0200

    KS-12-3 Resolved comments

--- src/main/kotlin/Lesson12Task3.kt
+++ src/main/kotlin/Lesson12Task3.kt
@@ -1,33 +1,29 @@
-class DailyWeatherDegrees {
-
-    private var daytimeTemperatureCelsius: Int = 0
-    private var nightTemperatureCelsius: Int = 0
-    private var precipitationDuringTheDay: Boolean = false
-
-    constructor(daytimeTemperatureKelvin: Int, nightTemperatureKelvin: Int, precipitationDuringTheDay: Boolean) {
-        daytimeTemperatureCelsius = convertKelvinInCelsius(daytimeTemperatureKelvin).toInt()
-        nightTemperatureCelsius = convertKelvinInCelsius(nightTemperatureKelvin).toInt()
-        this.precipitationDuringTheDay = precipitationDuringTheDay
-    }
+class DailyWeatherDegrees2(
+    daytimeTemperatureKelvin: Int,
+    nightTemperatureKelvin: Int,
+    precipitationDuringTheDay: Boolean,
+) {
+    private val daytimeTemperatureCelsius: Int = (daytimeTemperatureKelvin - ZERO_OFFSET).toInt()
+    private val nightTemperatureCelsius: Int = (nightTemperatureKelvin - ZERO_OFFSET).toInt()
+    private val isPrecipitationDuringTheDay: Boolean = precipitationDuringTheDay
 
     fun printWeather() {
         println("Daytime temperature is: ${daytimeTemperatureCelsius}°C")
         println("Night temperature is: ${nightTemperatureCelsius}°C")
-        println("Precipitation during the day: ${if (precipitationDuringTheDay) "yes" else "no"}")
+        println("Precipitation during the day: ${if (isPrecipitationDuringTheDay) "yes" else "no"}")
     }
 
-    private fun convertKelvinInCelsius(temperatureInKelvin: Int): Double {
-        return temperatureInKelvin - ZERO_OFFSET
+    private companion object {
+        const val ZERO_OFFSET = 273.15
     }
 }
 
-const val ZERO_OFFSET = 273.15
-
 fun main() {
-    val dailyWeather = DailyWeatherDegrees(
+    val dailyWeather = DailyWeatherDegrees2(
         daytimeTemperatureKelvin = 300,
         nightTemperatureKelvin = 290,
         precipitationDuringTheDay = true
     )
+
     dailyWeather.printWeather()
 }
