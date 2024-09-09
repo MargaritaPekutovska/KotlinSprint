f455b5baa5d9783e359f10a3634ee1f51a5cde65
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-09-09 17:51:50 +0200

    KS-16-2 Circle2

---
+++ src/main/kotlin/Lesson16Task2.kt
@@ -0,0 +1,21 @@
+class Circle2(private val radiusOfACircle: Double) {
+    companion object {
+        private const val PI = 3.14
+        private const val TWO = 2
+    }
+
+    fun circumference(): Double {
+        return TWO * PI * radiusOfACircle
+    }
+
+    fun areaOfACircle(): Double {
+        return PI * radiusOfACircle * radiusOfACircle
+    }
+}
+
+fun main() {
+    val circle = Circle2(5.0)
+
+    println("Circumference: ${circle.circumference()}")
+    println("Area: ${circle.areaOfACircle()}")
+}
