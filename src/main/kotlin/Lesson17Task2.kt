049f597258dd7e13c06a6988f05beba052e44707
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-09-11 21:18:55 +0200

    KS-17-2 Ship Game

---
+++ src/main/kotlin/Lesson17Task2.kt
@@ -0,0 +1,20 @@
+class Ship(
+    name: String,
+    averageSpeed: Int,
+    homePort: String,
+) {
+
+    var name: String = name
+        set(value) {
+            println("You can't change this name.")
+        }
+}
+
+fun main() {
+    val ship = Ship(
+        name = "Veronica",
+        averageSpeed = 80,
+        homePort = "Ningbo-Zhoushan"
+    )
+    ship.name = "New name"
+}
