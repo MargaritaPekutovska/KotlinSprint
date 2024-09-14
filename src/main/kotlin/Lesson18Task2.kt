e952bc34ad63d9220b7ee3531e89f2138069cfbc
Margarita Pekutovska <bondarchuk.margarita@gmail.com>
2024-09-14 12:29:54 +0200

    KS-18-2 resolved comment

--- src/main/kotlin/Lesson18Task2.kt
+++ src/main/kotlin/Lesson18Task2.kt
@@ -1,31 +1,15 @@
-import kotlin.random.Random
-import kotlin.random.nextInt
-
-interface Dice2 {
-    fun rollDice()
-}
-
-class FourSidedDice(private var randomNumberTill4: Int = 0) : Dice2 {
-    override fun rollDice() {
-        randomNumberTill4 = Random.nextInt(1, 5)
-        println("Your firs number is $randomNumberTill4")
-    }
-
-}
-
-class SixSidedDice(private var randomNumberTill6: Int = 0) : Dice2 {
-    override fun rollDice() {
-        randomNumberTill6 = Random.nextInt(1, 7)
-        println("Your second number is $randomNumberTill6")
+open class Dice2(private val numberOfSides: Int) {
+    fun rollDice() {
+        val rolledNumber = IntRange(1, numberOfSides).random()
+        println("Your firs number is $rolledNumber")
     }
 }
 
-class EightSidedDice(private var randomNumberTill8: Int = 0) : Dice2 {
-    override fun rollDice() {
-        randomNumberTill8 = Random.nextInt(1, 9)
-        println("Your third number is $randomNumberTill8")
-    }
-}
+class FourSidedDice : Dice2(numberOfSides = 4)
+
+class SixSidedDice : Dice2(numberOfSides = 6)
+
+class EightSidedDice : Dice2(numberOfSides = 8)
 
 fun main() {
     val listOfDice = arrayListOf<Dice2>(
